e3564477be566cf070aabf400321ed792176f25c
CameronMcDonna <P2431181@my365.dmu.ac.uk>
2020-03-19 21:31:54 +0000

    Created new test solution. Will need to re-create some of the other solutions and copy the code over if any is salvageable.

---
+++ ClothesTesting1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClothesTesting1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClothesTesting1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5415e70-395e-48bb-bd2f-97a53c2626d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ClothesTesting1/tstStock.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WidgteClasses;
+
+namespace ClothesTesting1
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //test to see if it exists
+            Assert.IsNotNull(SomeStock);
+        }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            SomeStock.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(SomeStock.Active, TestData);
+        }
+        public void GenderOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //create some test data to assign to the property
+            String TestData = "Male";
+            //assign the data to the property
+            SomeStock.Gender = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(SomeStock.DateArrived, TestData);
+        }
+
+        [TestMethod]
+        public void SizeOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //create some test data to assign to the property
+            String TestData = "M";
+            //assign the data to the property
+            SomeStock.Size = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(SomeStock.Size, TestData);
+        }
+        public void DescriptionOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //create some test data to assign to the property
+            String TestData = "A colourful t-shirt";
+            //assign the data to the property
+            SomeStock.Description = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(SomeStock.Description, TestData);
+        }
+        public void PriceOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            SomeStock.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(SomeStock.Price, TestData);
+        }
+        public void InStockOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //create some test data to assign to the property
+            bool TestData = true;
+            //assign the data to the property
+            SomeStock.InStock = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(SomeStock.InStock, TestData);
+        }
+        public void DateArrivedOK()
+        {
+            //create an instance of the class we want to create
+            clsStock SomeStock = new clsStock();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            SomeStock.DateArrived = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(SomeStock.DateArrived, TestData);
+        }
+
+    }
+}
